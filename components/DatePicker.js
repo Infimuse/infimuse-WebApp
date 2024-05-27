378d2d91d90d39f951c635935203338636cb4a4d
cjephuneh <calebjephuneh@gmail.com>
2024-05-27 16:46:13 +0300

    Refactor DatePicker component buttons

--- components/DatePicker.js
+++ components/DatePicker.js
@@ -13,12 +13,12 @@ const DatePickerComponent = () => {
 
   return (
     <div className="flex items-center justify-center mt-2 md:mt-0 text-black w-full">
-      <div className="flex items-center border-2 rounded-lg px-4 py-2 w-full md:w-96 lg:w-120 space-x-2 relative">
-        <button className="px-4 py-2 rounded-md hover:bg-gray-200">This Week</button>
-        <button className="px-4 py-2 rounded-md hover:bg-gray-200">Next Week</button>
-        <div className="relative">
+      <div className="flex items-center  rounded-lg px-4 py-2 w-full md:w-96 lg:w-120 relative">
+        <button className="px-4 py-2 mx-1 rounded-md bg-gray-200 hover:bg-gray-300 whitespace-nowrap">This Week</button>
+        <button className="px-4 py-2 mx-1 rounded-md bg-gray-200 hover:bg-gray-300 whitespace-nowrap">Next Week</button>
+        <div className="relative mx-1">
           <button
-            className="px-4 py-2 rounded-md hover:bg-gray-200"
+            className="px-4 py-2 rounded-md bg-gray-200 hover:bg-gray-300 whitespace-nowrap"
             onClick={() => setShowDatePicker(!showDatePicker)}
           >
             Select a date
@@ -34,7 +34,7 @@ const DatePickerComponent = () => {
             </div>
           )}
         </div>
-        <button className="px-4 py-2 rounded-md hover:bg-gray-200">Reset</button>
+        <button className="px-4 py-2 mx-1 rounded-md bg-gray-200 hover:bg-gray-300 whitespace-nowrap">Reset</button>
       </div>
     </div>
   );
