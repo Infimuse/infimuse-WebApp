3371d9ef86edf1821b9b7efef30389dc8b253b5d
cjephuneh <calebjephuneh@gmail.com>
2024-06-01 13:24:33 +0300

    Refactor Card component layout and add ellipsis for long titles

--- components/Card.js
+++ components/Card.js
@@ -9,24 +9,25 @@ const Card = ({ id, image, title, host, status, price, rating, slotsBooked }) =>
   ));
 
   return (
-    <div className="block bg-white rounded-lg shadow-md overflow-hidden" style={{ minWidth: '250px', minHeight: '400px' }}>
+    <div className="block bg-white rounded-lg shadow-md overflow-hidden" style={{ minWidth: '250px' }}>
       <Link href={`/experience/${id}`}>
         <div className="relative w-full h-40">
           <Image src={image} alt={title} layout="fill" objectFit="cover" />
         </div>
       </Link>
-      <div className="p-4">
-        <h3 className="text-lg font-semibold">{title}</h3>
-        <p className="text-gray-500">Hosted by {host}</p>
-        <div className="flex items-center">
-          {ratingStars}
-          <span className="ml-2">{rating} / 5</span>
+      <div className="p-4 h-48 flex flex-col justify-between">
+        <div>
+          <h3 className="text-lg font-semibold overflow-hidden overflow-ellipsis whitespace-nowrap">{title}</h3>
+          <p className="text-gray-500 overflow-hidden overflow-ellipsis whitespace-nowrap">Hosted by {host}</p>
+          <div className="flex items-center">
+            {ratingStars}
+            <span className="ml-2">{rating} / 5</span>
+          </div>
+          <p className="text-gray-500">{price}</p>
+          <p className="text-gray-500 mb-1">Slots booked: {slotsBooked}</p>
         </div>
-        {/* <p className={`text-${status === 'Sold out' ? 'red' : 'green'}-500`}>{status}</p> */}
-        <p className="text-gray-500">{price}</p>
-        <p className="text-gray-500">Slots booked: {slotsBooked}</p>
         <Link href={`/experience/${id}`}>
-          <button className="mt-2 w-full bg-blue-500 text-white py-2 rounded-lg">
+          <button className="w-full bg-blue-500 text-white py-2 rounded-lg">
             Book Now
           </button>
         </Link>
